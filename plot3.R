5eac492ddebd2a1986125e2fa59f043337b82fc2
Gary Tong <gtong305@gmail.com>
2017-01-26 15:04:38 -0800

    Resolve legend issue with plot3

--- plot3.R
+++ plot3.R
@@ -11,13 +11,13 @@ raw_data$datetime <- dmy_hms(raw_data$datetime) ## convert to datetime format
 
 clean_data <- raw_data[grepl("2007-02-01|2007-02-02", raw_data$datetime),]
 
-par(xpd = TRUE, mar = c(5.1, 4.1, 4.1, 3.1))
+png(filename = "plot3.png", units = "px", width = 480, height = 480, bg = "white")
+
 with(clean_data, plot(datetime, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = ""))
 with(clean_data, points(datetime, Sub_metering_2, type = "l", col = "red"))
 with(clean_data, points(datetime, Sub_metering_3, type = "l", col = "blue"))
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
 
-dev.copy(png, file = "plot3.png")
 dev.off()
 
 unlink(temp)
